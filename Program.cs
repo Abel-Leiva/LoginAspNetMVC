c02392f37d839b82c5c24efc95feab452afc0b6e
Abel Leiva <a.ignacio.leivaleiva@gmail.com>
2025-06-23 03:28:36 -0300

    migracionInicial

--- Program.cs
+++ Program.cs
@@ -31,7 +31,11 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
     });
 
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
